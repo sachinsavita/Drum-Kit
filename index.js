0294c7711a4052f99077f2bbd1062fc01551a603
sachinsavita <sachinsavita95@gmail.com>
2022-07-27 13:36:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrumButton;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click", handleclick);  //why not calling function handClick with () //If we will mention the () the it will be called as the file loaded not when click.
+function handleclick(){
+
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+      
+}
+}
+
+
+
+
+document.addEventListener("keypress", function(event){
+
+    makeSound(event.key);
+
+});
+
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3");
+                var tom1 = audio.play();
+            break;
+        case "a":
+                var audio = new Audio("./sounds/tom-2.mp3");
+                var tom2 = audio.play();
+                break;    
+        case "s":
+                    var audio = new Audio("./sounds/tom-3.mp3");
+                var tom3 = audio.play();
+                    break; 
+        case "d":
+                    var audio = new Audio("./sounds/tom-4.mp3");
+                var tom4 =  audio.play();
+                    break; 
+        case "j":
+                    var audio = new Audio("./sounds/snare.mp3");
+                var snare = audio.play();
+                    break; 
+        case "k":
+                    var audio = new Audio("./sounds/crash.mp3");
+                var crash = audio.play();
+                    break; 
+        case "l":
+                    var audio = new Audio("./sounds/kick-bass.mp3");
+                var kick = audio.play();
+                    break; 
+        default:
+            console.log(buttonInnerHTML);
+    } 
+}
+
+
+
